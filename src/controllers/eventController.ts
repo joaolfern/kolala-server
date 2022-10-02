68186bc5a33ffa8e2bf5b22d1a957dcd096651f8
João Luiz <joaoaoluiz@protonmail.com>
2022-10-01 22:48:25 -0300

    Fix createEvent

--- src/controllers/eventController.ts
+++ src/controllers/eventController.ts
@@ -1,14 +1,8 @@
 import { Event, EventImage } from '@prisma/client'
+import dayjs from 'dayjs'
 import { Multer } from 'multer'
 import { client } from '..'
 import { AuthRequest, AuthResponse, BodyRequest } from '../types/types'
-
-function checkStorageEnv(file) {
-  return {
-    key: file.key,
-    url: file.location || `${process.env.API_URL}/files/${this.key}`,
-  }
-}
 
 const eventController = {
   index: async (req: BodyRequest<undefined>, res: AuthResponse<any>) => {
@@ -20,19 +14,22 @@ const eventController = {
       res.status(400).json(err)
     }
   },
-  create: async (req: BodyRequest<Event> & AuthRequest, res: AuthResponse<any>) => {
-    const { category, lat, lng, status, datetime, icon, ...eventRest } = req.body
+  create: async (
+    req: BodyRequest<Event> & AuthRequest,
+    res: AuthResponse<any>
+  ) => {
+    const { category, lat, lng, status, datetime, icon, ...eventRest } =
+      req.body
     const { userId: authorId } = req
 
-    const data: Event = {
+    const data: Omit<Event, 'status'> = {
       ...eventRest,
       lat: parseFloat(String(lat)),
       lng: parseFloat(String(lng)),
-      category: parseInt(String(category)),
-      icon: parseInt(String(category)),
-      // @ts-ignore
-      datetime: new Date(datetime || Date.now()).toISOString(),
-      authorId
+      category: Number(String(category)),
+      icon: Number(String(category)),
+      authorId: authorId,
+      datetime: new Date(datetime),
     }
 
     try {
@@ -40,17 +37,20 @@ const eventController = {
         data,
       })
       const { id: eventId } = createdEvent
+
       const formattedImages: EventImage[] = Array.isArray(req.files)
         ? (req.files.map((file: any) => ({
-            key: file.key,
-            url: file.location || `${process.env.API_URL}/files/${file.key}`,
+            key: file.key || file.originalname,
+            url:
+              file.location ||
+              `${process.env.API_URL}/files/${file.key || file.originalname}`,
             eventId,
           })) as unknown as EventImage[])
         : ([] as EventImage[])
 
-        client.eventImage.createMany({
-          data: formattedImages
-        })
+      await client.eventImage.createMany({
+        data: formattedImages,
+      })
 
       res.status(200).json('success')
     } catch (err) {
