b3ae13b7ddafa60003b637dcc4001a30878c3c0f
João Luiz <joaoaoluiz@protonmail.com>
2022-10-13 22:12:51 -0300

    Add get profile

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -11,7 +11,24 @@ const userController = {
       console.log(err)
       res.status(400).json(err)
     }
-  }
+  },
+  profile: async (req: Request<{id: string}>, res: AuthResponse<any>) => {
+    const { id } = req.params
+    const parsedId = Number(id)
+
+    try {
+      const data = await prisma.profile.findUnique({
+        where: {
+          id: parsedId
+        }
+      })
+
+      res.status(200).json({ data })
+    } catch (err) {
+      console.log(err)
+      res.status(400).json(err)
+    }
+  },
 }
 
 export default userController
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -4,6 +4,7 @@ import userController from '../controllers/userController'
 const userRouter = Router()
 
 userRouter.get('/', userController.index)
+userRouter.get('/profile/:id', userController.profile)
 
 
 export default userRouter
